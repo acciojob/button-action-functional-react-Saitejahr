c95edc2dd500e15503ff8d306c6741e2a79d8da7
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-05-14 15:11:14 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { Component, useState } from 'react'
+import './../styles/App.css'
 
 function App() {
+  const [isVisible, setVisible] = useState(false)
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={() => setVisible(true)} id="click">
+        Click Me
+      </button>
+      {isVisible ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
